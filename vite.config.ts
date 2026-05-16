01f4f63a7efd7ae0ff4a6059a9483a6b20038201
kt474 <kt474@cornell.edu>
2026-05-16 12:55:29 -0400

    Setup proxy for onyx api

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/onyx": {
+        target: "https://predictions.dev-onyxodds.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/onyx/, ""),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
